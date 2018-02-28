a980db7a21acb1b54024520478647ed3f778e88a
Daniel Waters <danwater1@gmail.com>
2018-02-27 22:59:22 -0800

    Finished code

--- Sudoku.c
+++ Sudoku.c
@@ -9,95 +9,153 @@
 #include <pthread.h>
 #include <stdio.h>
 
-int puzzle[] = {
-			6, 5, 3, 1, 2, 8, 7, 9, 4,
-			1, 7, 4, 3, 5, 9, 6, 8, 2,
-			9, 2, 8, 4, 6, 7, 5, 3, 1,
-			2, 8, 6, 5, 1, 4, 3, 7, 9,
-			3, 9, 1, 7, 8, 2, 4, 5, 6,
-			5, 4, 7, 6, 9, 3, 2, 1, 8,
-			8, 6, 5, 2, 3, 1, 9, 4, 7,
-			4, 1, 2, 9, 7, 5, 8, 6, 3,
-			7, 3, 9, 8, 4, 6, 1, 2, 5
+#define MAX_THREADS 27
+
+int puzzle[9][9] = {
+			{6, 5, 3, 1, 2, 8, 7, 9, 4},
+			{1, 7, 4, 3, 5, 9, 6, 8, 2},
+			{9, 2, 8, 4, 6, 7, 5, 3, 1},
+			{2, 8, 6, 5, 1, 4, 3, 7, 9},
+			{3, 9, 1, 7, 8, 2, 4, 5, 6},
+			{5, 4, 7, 6, 9, 3, 2, 1, 8},
+			{8, 6, 5, 2, 3, 1, 9, 4, 7},
+			{4, 1, 2, 9, 7, 5, 8, 6, 3},
+			{7, 3, 9, 8, 4, 6, 1, 2, 5}
 	};
 
-int validity[81] = { 0 };
+typedef struct {
+	int row;
+	int col;
+	int result;
+} parameters;
 
-void *checkRow(void *param);
-void *checkCol(void *param);
-void *checkBox(void *param);
+void *checkRow(void*);
+void *checkCol(void*);
+void *checkBox(void*);
+int checkLocation(int, int, int);
+void printArray();
+int checkResults(parameters*);
 
 int main() {
 
-	pthread_t tid[3][9];
+	parameters *data = (parameters *) malloc(MAX_THREADS * sizeof(parameters));
+
+	pthread_t tid[MAX_THREADS];
 	pthread_attr_t attr;
 	pthread_attr_init(&attr);
-	int rowLocations[] = {0, 9, 18, 27, 36, 45, 54, 63, 72};
-	int colLocations[] = {0, 1, 2, 3, 4, 5, 6, 7, 8};
-	int boxLocations[] = {0, 3, 6, 27, 30, 33, 54, 47, 60};
 
 	for (int i = 0; i < 9; i++) {
-		pthread_create(&tid[0][i], &attr, checkRow, &rowLocations[i]);
-		pthread_create(&tid[1][i], &attr, checkCol, &colLocations[i]);
-		pthread_create(&tid[2][i], &attr, checkBox, &boxLocations[i]);
+		data[i].result = 0;
+		data[i].row = i;
+		data[i+9].col = i;
+		data[i+9].result = 0;
+		pthread_create(&tid[i], &attr, checkRow, &data[i]);
+		pthread_create(&tid[i+9], &attr, checkCol, &data[i+9]);
 	}
 
-	for (int i = 0; i < 3; i++) {
-		for (int j = 0; j < 9; j++) {
-			pthread_join(tid[i][j], NULL);
+	int index = 18;
+	for (int i = 0; i < 9; i+=3) {
+		for (int j = 0; j < 9; j+=3) {
+			data[index].result = 0;
+			data[index].row = i;
+			data[index].col = j;
+			pthread_create(&tid[index], &attr, checkBox, &data[index]);
+			index++;
 		}
 	}
 
-	for (int i = 1; i < 82; i++) {
-		printf("%d ", validity[i-1]);
-		if (i != 0 && i % 9 == 0) {
-			printf("\n");
-		}
+	for (int i = 0; i < 27; i++) {
+		pthread_join(tid[i], NULL);
 	}
+
+	printArray();
+
+	if (checkResults(data)) {
+		printf("Invalid Sudoku.\n");
+	}
+
+	free(data);
+
 	return 0;
 }
 
 void *checkRow(void *params) {
+	parameters *param = (parameters*) params;
+	int row = param->row;
 
-	int location = *(int*) params;
-	for (int i = location; i < location+9; i++) {
+	for (int i = 0; i < 9; i++) {
 		int occurances = 0;
-		for (int j = location; j < location+9; j++) {
-			if(puzzle[i] == puzzle[j]) {
+		for (int j = 0; j < 9; j++) {
+			if(puzzle[row][i] == puzzle[row][j]) {
 				occurances++;
 			}
 		}
 		if (occurances != 1) {
-			validity[i] = 1;
+			param->result = 1;
 		}
 	}
 	pthread_exit(0);
 }
 
 void *checkCol(void *params) {
-	int location = *(int*) params;
-	for (int i = location; i < 81; i+=9) {
+	parameters *param = (parameters*) params;
+	int col = param->col;
 
+	for (int i = 0; i < 9; i++) {
 		int occurances = 0;
-
-		for (int j = location; j < 81; j+=9) {
-			if(puzzle[i] == puzzle[j]) {
+		for (int j = 0; j < 9; j++) {
+			if(puzzle[i][col] == puzzle[j][col]) {
 				occurances++;
 			}
 		}
 		if (occurances != 1) {
-			validity[i] = 1;
+			param->result = 1;
 		}
 	}
 	pthread_exit(0);
 }
 
-void *checkSquare(void *params) {
+void *checkBox(void *params) {
+	parameters *param = (parameters*) params;
+	int row = param->row;
+	int col = param->col;
 
-	int location = *(int*) params;
-
+	for (int i = row; i < (row+3); i++) {
+		for (int j = col; j < (col+3); j++) {
+			if(checkLocation(puzzle[i][j], row, col) != 1) {
+				param->result = 1;
+			}
+		}
+	}
 	pthread_exit(0);
 }
 
+int checkLocation(int value, int row, int col) {
+	int occurances = 0;
+	for (int i = row; i < (row + 3); i++) {
+		for (int j = col; j < (col +3); j++) {
+			if (value == puzzle[i][j]) {
+				occurances++;
+			}
+		}
+	}
+	return occurances;
+}
 
+void printArray() {
+	for (int i = 0; i < 9; i++) {
+		for (int j = 0; j < 9; j++) {
+			printf("%d ", puzzle[i][j]);
+		}
+		printf("\n");
+	}
+}
 
+int checkResults(parameters *data) {
+	for (int i = 0; i < 27; i++) {
+		if (data[i].result != 0) {
+			return 1;
+		}
+	}
+	return 0;
+}
